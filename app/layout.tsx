1711034ee2b9c9ba5774d612214d5dcad001015b
Patrik Michalcak <michalcak@pmgslovakia.sk>
2026-06-13 21:01:40 +0200

    Pridane Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -64,7 +65,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="sk">
-      <body>{children}</body>
+      <body>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-6KX27T9W8G"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-6KX27T9W8G');
+          `}
+        </Script>
+
+        {children}
+      </body>
     </html>
   );
 }
